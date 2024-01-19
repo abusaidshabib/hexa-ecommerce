1f62c579e36f7893d67084eb2f8c8edd3861c5c4
Your Name <abusaidshabib712@gmail.com>
2024-01-20 00:56:30 +0600

    version_1 shift

--- Frontend-v2/src/components/Important/Logo.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-const Logo = ({ css }) => {
-  return (
-    <p className={`uppercase font-young text-2xl font-bold ${css}`}>Gamazone</p>
-  );
-};
-
-export default Logo;
